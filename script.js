ffff3c27daf4a388c284b6bc4fbc3dd77d5661a6
tigermarg <178945478+tigermarg@users.noreply.github.com>
2024-09-06 17:13:07 -0400

    Finished Part One

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+// const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
@@ -49,9 +49,8 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 
 
 //   Check if all numbers are divisible by 5. Cache the result in a variable.
-// const divByFive = n1 % 5 === 0 && n2 % 5 === 0 && n3 % 5 === 0 && n4 % 5 === 0 
-// const divByFive = (n1 + n2 + n3 + n4 ) % 5 == 0
-// console.log(`Are div by five?` , divByFive);
+const divByFive = (n1 + n2 + n3 + n4 ) % 5 == 0
+console.log(`Are div by five?` , divByFive); // True
 
 
 //   Check if the first number is larger than the last. Cache the result in a variable.
@@ -73,7 +72,8 @@ let resDivFourth = (-100 % n4);
 console.log(`Result divided by fourth remainder?` , resDivFourth) // Equals 0
 
 //   Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
-
+const isOver25 = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25; // Set all values less than 25
+console.log(`Numbers over 25?`, isOver25)
 
 // Part Two: Practical Math ----------------------------------------------------------
 // Part Three: Future Explorations ----------------------------------------------------
